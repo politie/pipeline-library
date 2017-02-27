bb996e6829799636bf368700ec3ba3b158c903ac
rkettelerij <richard@mindloops.nl>
2017-02-27 17:25:35 +0100

    updated pipeline library (removed unused constant, new stage syntax)

--- src/politie/jenkins/Constants.groovy
+++ src/politie/jenkins/Constants.groovy
@@ -9,7 +9,6 @@ class Constants {
     static final GITLAB_WEB_BASE_URL = 'enter gitlab url here';
     static final GITLAB_API_BASE_URL = 'enter gitlab api url here';
     static final GITLAB_CHECKOUT_BASE_URL = 'enter gitlab checkout url here';
-    static final GITLAB_BUILDS_URL = GITLAB_CHECKOUT_BASE_URL + 'builds.git';
     static final GITLAB_VERSION = '8.6';
     static final GITLAB_CREDENTIALS_ID = 'enter credentials here';
     static final GITLAB_API_TOKEN_CREDENTIALS_ID = 'enter api token here';
--- src/politie/jenkins/JenkinsPipelineBuilder.groovy
+++ src/politie/jenkins/JenkinsPipelineBuilder.groovy
@@ -16,48 +16,53 @@ def getPipelineSteps() {
 void mavenPipeline(String deployType, String nodeLabel, String serviceName, String defaultBranch, boolean enableJacoco) {
     node(nodeLabel) {
         try {
-            stage 'Checkout'
-            pipelineSteps.cleanWorkspace()
-            pipelineSteps.checkout(serviceName, defaultBranch)
+            stage('Checkout') {
+                pipelineSteps.cleanWorkspace()
+                pipelineSteps.checkout(serviceName, defaultBranch)
+            }
 
-            stage 'Configure'
             String version = pipelineSteps.determineVersion()
             String branch = pipelineSteps.getBranch()
-            pipelineSteps.setBuildName(version, branch)
-            pipelineSteps.setBuildProperties()
+            stage('Configure') {
+                pipelineSteps.setBuildName(version, branch)
+                pipelineSteps.setBuildProperties()
+            }
 
-            stage 'Version'
-            pipelineSteps.updateApplicationVersionMaven(version)
+            stage('Version') {
+                pipelineSteps.updateApplicationVersionMaven(version)
+            }
 
             gitlabCommitStatus('jenkins') {
-                stage name: 'Build', concurrency: 1
-                pipelineSteps.runMavenBuild(enableJacoco)
+                stage('Build') {
+                    pipelineSteps.runMavenBuild(enableJacoco)
 
-                if (branch == 'master') {
-                    stage 'Sonar'
-                    pipelineSteps.runSonarAnalysis();
-
-                    stage 'Publish'
-                    pipelineSteps.deployToNexus();
-
-                    def commitId = pipelineSteps.getCommitId()
-                    pipelineSteps.tagCommit(serviceName, commitId, version)
-
-                    if (deployType != 'library') {
-                        // libraries don't need a deployment step
-                        stage 'Deploy'
-                        if (deployType == 'application') {
-                            pipelineSteps.deployApplicationWithConfigToDevEnv(serviceName, version, 'springboot');
-                            pipelineSteps.checkDeployedVersion(serviceName, version, Constants.ENVIRONMENT_TENANT);
-                        } else if (deployType == 'sparkjob') {
-                            pipelineSteps.deploySparkJobToDevEnv(serviceName, version);
-                        } else {
-                            error 'Unknown deployType "' + deployType + '"!'
-                            currentBuild.result = "FAILED"
+                    if (branch == 'master') {
+                        stage('Sonar') {
+                            pipelineSteps.runSonarAnalysis();
                         }
+                        stage('Publish') {
+                            pipelineSteps.deployToNexus();
+                        }
+                        
+                        def commitId = pipelineSteps.getCommitId()
+                        pipelineSteps.tagCommit(serviceName, commitId, version)
+
+                        if (deployType != 'library') { // libraries don't need a deployment step
+                            stage ('Deploy') {
+                                if (deployType == 'application') {
+                                    pipelineSteps.deployApplicationWithConfigToDevEnv(serviceName, version, 'springboot');
+                                    pipelineSteps.checkDeployedVersion(serviceName, version, Constants.ENVIRONMENT_TENANT);
+                                } else if (deployType == 'sparkjob') {
+                                    pipelineSteps.deploySparkJobToDevEnv(serviceName, version);
+                                } else {
+                                    error 'Unknown deployType "' + deployType + '"!'
+                                    currentBuild.result = "FAILED"
+                                }
+                            }
+                        }
+                    } else {
+                        println 'Not on master branch, skipping....'
                     }
-                } else {
-                    println 'Not on master branch, skipping....'
                 }
             }
         } catch (e) {
@@ -66,7 +71,6 @@ void mavenPipeline(String deployType, String nodeLabel, String serviceName, Stri
         }
     }
 }
-
 
 // Convenience methods for the generic maven pipeline
 
